e721c33409ca0ef3d2211e29fa586dfb227cfb7e
Oksana Semonenko <ok.semonenko@gmail.com>
2016-08-03 23:09:30 +0300

    Добавляет подсветку активного пункта списка

--- js/controllers/ClickCtrl.js
+++ js/controllers/ClickCtrl.js
@@ -7,6 +7,7 @@
 	function ClickCtrl() {
 		// Variables - Private
 		var self = this;
+		var selectedItem;
 
 		// Variables - Public
 		self.items = [
@@ -413,8 +414,9 @@
 		];
 
 		// Functions - Public
-
 		self.clickItem = clickItem;
+		self.clickLi = clickLi;
+		self.highlight = highlight;
 
 		// Functions - Definitions
 		function clickItem(item) {
@@ -427,10 +429,27 @@
 			self.flag2 = item.flags[1];
 			self.flag3 = item.flags[2];
 			self.flag4 = item.flags[3];
-			// for (var i = 0; i <= 4, i++;) {
-			// 	self.flag = item.flags[i];
-			// 	self.class = item.flags[i];
-			// }
+			self.class = "active";
+		}
+
+		function clickLi(event) {
+			var target = event.target;
+			while (target != self) {
+				if (target.tagName == 'LI') {
+					highlight(target);
+					return;
+				}
+				target = target.parentNode;
+				highlight(target);
+			}
+		}
+
+		function highlight(node) {
+			if (selectedItem) {
+				selectedItem.classList.remove('items__item--active');
+			}
+			selectedItem = node;
+			selectedItem.classList.add('items__item--active');
 		}
 	}
 }());
