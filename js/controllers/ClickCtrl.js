73eb344930c2e0332ffb8eba53e011b537858080
Oksana Semonenko <ok.semonenko@gmail.com>
2016-08-03 20:37:08 +0300

    Правки списка элементов: добавление до 100

--- js/controllers/ClickCtrl.js
+++ js/controllers/ClickCtrl.js
@@ -12,6 +12,66 @@
 		self.items = [
 			{
 				name: "cat",
+				flags: ["heart"]
+			},
+			{
+				name: "mouse",
+				flags: ["heart", "sun", "flash"]
+			},
+			{
+				name: "dog",
+				flags: ["sun", "flash"]
+			},
+			{
+				name: "fox",
+				flags: ["flower", "sun", "flash"]
+			},
+			{
+				name: "horse",
+				flags: ["flower", "heart", "flash"]
+			},
+			{
+				name: "rabbit",
+				flags: []
+			},
+			{
+				name: "crocodile",
+				flags: ["flower", "heart", "sun"]
+			},
+			{
+				name: "zebra",
+				flags: ["flower"]
+			},
+			{
+				name: "wombat",
+				flags: ["heart", "sun"]
+			},
+			{
+				name: "raccoon",
+				flags: ["sun"]
+			},
+			{
+				name: "pig",
+				flags: ["flash"]
+			},
+			{
+				name: "panda",
+				flags: ["flower", "flash"]
+			},
+			{
+				name: "oyster",
+				flags: ["heart", "sun"]
+			},
+			{
+				name: "alpaca",
+				flags: ["flower", "heart", "sun"]
+			},
+			{
+				name: "antelope",
+				flags: ["flower"]
+			},
+			{
+				name: "cat",
 				flags: ["flower", "flash"]
 			},
 			{
@@ -37,6 +97,318 @@
 			{
 				name: "crocodile",
 				flags: ["flower", "heart", "sun"]
+			},
+			{
+				name: "zebra",
+				flags: ["flower"]
+			},
+			{
+				name: "wombat",
+				flags: ["heart", "sun"]
+			},
+			{
+				name: "raccoon",
+				flags: ["sun"]
+			},
+			{
+				name: "pig",
+				flags: ["flash"]
+			},
+			{
+				name: "panda",
+				flags: ["flower", "flash"]
+			},
+			{
+				name: "oyster",
+				flags: ["heart", "sun"]
+			},
+			{
+				name: "alpaca",
+				flags: ["flower", "heart", "sun"]
+			},
+			{
+				name: "antelope",
+				flags: ["flower"]
+			},
+			{
+				name: "cat",
+				flags: ["flower", "flash"]
+			},
+			{
+				name: "mouse",
+				flags: ["heart", "sun", "flash"]
+			},
+			{
+				name: "dog",
+				flags: ["sun", "flash"]
+			},
+			{
+				name: "fox",
+				flags: ["flower", "sun", "flash"]
+			},
+			{
+				name: "horse",
+				flags: ["flower", "heart", "flash"]
+			},
+			{
+				name: "rabbit",
+				flags: ["flower", "heart", "sun", "flash"]
+			},
+			{
+				name: "crocodile",
+				flags: ["flower", "heart", "sun"]
+			},
+			{
+				name: "zebra",
+				flags: ["flower"]
+			},
+			{
+				name: "wombat",
+				flags: ["heart", "sun"]
+			},
+			{
+				name: "raccoon",
+				flags: ["sun"]
+			},
+			{
+				name: "pig",
+				flags: ["flash"]
+			},
+			{
+				name: "panda",
+				flags: ["flower", "flash"]
+			},
+			{
+				name: "oyster",
+				flags: ["heart", "sun"]
+			},
+			{
+				name: "alpaca",
+				flags: ["flower", "heart", "sun"]
+			},
+			{
+				name: "antelope",
+				flags: ["flower"]
+			},
+			{
+				name: "cat",
+				flags: ["flower", "flash"]
+			},
+			{
+				name: "mouse",
+				flags: ["heart", "sun", "flash"]
+			},
+			{
+				name: "dog",
+				flags: ["sun", "flash"]
+			},
+			{
+				name: "fox",
+				flags: ["flower", "sun", "flash"]
+			},
+			{
+				name: "horse",
+				flags: ["flower", "heart", "flash"]
+			},
+			{
+				name: "rabbit",
+				flags: ["flower", "heart", "sun", "flash"]
+			},
+			{
+				name: "crocodile",
+				flags: ["flower", "heart", "sun"]
+			},
+			{
+				name: "zebra",
+				flags: ["flower"]
+			},
+			{
+				name: "wombat",
+				flags: ["heart", "sun"]
+			},
+			{
+				name: "raccoon",
+				flags: ["sun"]
+			},
+			{
+				name: "pig",
+				flags: ["flash"]
+			},
+			{
+				name: "panda",
+				flags: ["flower", "flash"]
+			},
+			{
+				name: "oyster",
+				flags: ["heart", "sun"]
+			},
+			{
+				name: "alpaca",
+				flags: ["flower", "heart", "sun"]
+			},
+			{
+				name: "antelope",
+				flags: ["flower"]
+			},
+			{
+				name: "cat",
+				flags: ["flower", "flash"]
+			},
+			{
+				name: "mouse",
+				flags: ["heart", "sun", "flash"]
+			},
+			{
+				name: "dog",
+				flags: ["sun", "flash"]
+			},
+			{
+				name: "fox",
+				flags: ["flower", "sun", "flash"]
+			},
+			{
+				name: "horse",
+				flags: ["flower", "heart", "flash"]
+			},
+			{
+				name: "rabbit",
+				flags: ["flash"]
+			},
+			{
+				name: "crocodile",
+				flags: ["flower", "heart", "sun"]
+			},
+			{
+				name: "zebra",
+				flags: ["flower"]
+			},
+			{
+				name: "wombat",
+				flags: ["heart", "sun"]
+			},
+			{
+				name: "raccoon",
+				flags: ["sun"]
+			},
+			{
+				name: "pig",
+				flags: ["flash"]
+			},
+			{
+				name: "panda",
+				flags: ["flower", "flash"]
+			},
+			{
+				name: "oyster",
+				flags: ["heart", "sun"]
+			},
+			{
+				name: "alpaca",
+				flags: ["flower", "heart", "sun"]
+			},
+			{
+				name: "antelope",
+				flags: ["flower"]
+			},
+			{
+				name: "cat",
+				flags: ["flower", "flash"]
+			},
+			{
+				name: "mouse",
+				flags: ["heart", "sun", "flash"]
+			},
+			{
+				name: "dog",
+				flags: ["sun"]
+			},
+			{
+				name: "fox",
+				flags: ["flower"]
+			},
+			{
+				name: "horse",
+				flags: ["flower", "heart", "flash"]
+			},
+			{
+				name: "rabbit",
+				flags: ["flower", "flash"]
+			},
+			{
+				name: "crocodile",
+				flags: ["flower", "heart", "sun"]
+			},
+			{
+				name: "zebra",
+				flags: ["flower"]
+			},
+			{
+				name: "wombat",
+				flags: ["heart", "sun"]
+			},
+			{
+				name: "raccoon",
+				flags: ["sun"]
+			},
+			{
+				name: "pig",
+				flags: ["flash"]
+			},
+			{
+				name: "panda",
+				flags: ["flower", "flash"]
+			},
+			{
+				name: "oyster",
+				flags: ["heart"]
+			},
+			{
+				name: "alpaca",
+				flags: ["flower", "sun"]
+			},
+			{
+				name: "antelope",
+				flags: ["flower"]
+			},
+			{
+				name: "rabbit",
+				flags: ["sun", "flash"]
+			},
+			{
+				name: "crocodile",
+				flags: [ "sun"]
+			},
+			{
+				name: "zebra",
+				flags: ["flower"]
+			},
+			{
+				name: "wombat",
+				flags: ["heart", "sun"]
+			},
+			{
+				name: "raccoon",
+				flags: ["sun"]
+			},
+			{
+				name: "pig",
+				flags: ["flash"]
+			},
+			{
+				name: "panda",
+				flags: ["flower", "flash"]
+			},
+			{
+				name: "oyster",
+				flags: ["heart", "sun"]
+			},
+			{
+				name: "alpaca",
+				flags: ["flower", "sun"]
+			},
+			{
+				name: "antelope",
+				flags: ["flower"]
 			}
 		];
 
@@ -48,8 +420,17 @@
 		function clickItem(item) {
 			self.msg = "Clicked: " + item.name;
 			self.info = "Info";
-			self.name = "Item: " + item.name;
-			self.flags = "Flags: " + item.flags;
+			self.name = "Name : ";
+			self.itemName = item.name;
+			self.flags = "Flags : ";
+			self.flag1 = item.flags[0];
+			self.flag2 = item.flags[1];
+			self.flag3 = item.flags[2];
+			self.flag4 = item.flags[3];
+			// for (var i = 0; i <= 4, i++;) {
+			// 	self.flag = item.flags[i];
+			// 	self.class = item.flags[i];
+			// }
 		}
 	}
 }());
